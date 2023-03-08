3dc82b8456cd568de2d2334133272ef77e38e37c
Scott Harrison <scottrules44@gmail.com>
2023-03-07 22:13:22 -0600

    Updating Adapters for Max 11.8.1

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -3,9 +3,14 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { "MyTargetAdapter" },
-		frameworks = { "MyTargetSDK" },
+
+		-- This is the name without the 'lib' prefix.
+		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
+		staticLibs = {"MyTargetSDK", "MyTargetAdapter", "objc"},
+
+		frameworks = { "CoreLocation", "CoreTelephony", "CoreMedia", "SystemConfiguration", "StoreKit", "AdSupport", "SafariServices", "AVFoundation"},
 		frameworksOptional = {},
+		usesSwift = true,
 	},
 }
 
